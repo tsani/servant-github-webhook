3168f01a0fee56235ef5060012c207787190c553
Thomas M. DuBuisson <tommd@galois.com>
2018-01-23 10:12:47 -0800

    Add a dynamic key capability based on 'result'
    Some uses of webhooks require dynamic keys, such as adding new keys for
    new users or repositories.  This change allows dynamic keys to look at
    the result (ex: repository or username) before returning a key for use
    in verification.  N.B. the API changes since we now have:
    
    Another type variable on GitHubKey'.
    GitHubKey now wraps a function that takes an extra argument.
    Helper functions of `dynamicKey` and `repositoryKey` exist.
    Helper class of `HasRepository` exists with bare-bones instances.

--- test/dynamickey/Main.hs
+++ test/dynamickey/Main.hs
@@ -4,12 +4,14 @@
 
 import Control.Monad.IO.Class ( liftIO )
 import Data.Aeson ( Object )
+import Data.Monoid
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
 import Servant
 import Servant.GitHub.Webhook
 import Network.Wai ( Application )
 import Network.Wai.Handler.Warp ( run )
+import qualified Data.Text.Encoding as T
 
 main :: IO ()
 main = pure ()
@@ -17,7 +19,7 @@ main = pure ()
 realMain :: IO ()
 realMain = do
   [key, _] <- C8.lines <$> BS.readFile "test/test-keys"
-  run 8080 (app (gitHubKey $ pure key))
+  run 8080 (app (repositoryKey $ \user -> pure $ Just (T.encodeUtf8 user <> key)))
 
 app :: GitHubKey Object -> Application
 app key
